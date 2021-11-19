83fbe2bd76217a498d922c57468a4a2bdfe9f95d
Lazarev Ilia <crusadergo21@gmail.com>
2021-11-19 13:35:18 +0100

    Add token

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -3,6 +3,7 @@ module Mutations
         argument :params, Types::Input::UserInputType, required: true
 
         field :user, Types::UserType, null: false
+        field :token, String, null: true
 
         def resolve(params:)
             user_params = Hash(params)
@@ -10,10 +11,14 @@ module Mutations
             begin
                 user = User.create!(user_params)
 
+                crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+                token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+                context[:session][:token] = token
+
                 { user: {
                     id: user.id,
                     login: user.login
-                } }
+                }, token: token }
             rescue ActiveRecord::RecordInvalid => e
                 GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
                 " #{e.record.errors.full_messages.join(', ')}")
